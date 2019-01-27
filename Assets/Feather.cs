7e8fdcc02fd702ebc4d21ba8345ccea1d5a91b9c
Erik Pukinskis <erik@snowedin.net>
2019-01-27 15:23:32 -0800

    Basic Unity3d project that paints a triangle

---
+++ Assets/Feather.cs
@@ -0,0 +1,33 @@
+﻿// Builds a Mesh containing a single triangle with uvs.
+// Create arrays of vertices, uvs and triangles, and copy them into the mesh.
+
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class Feather : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+    void Awake () {
+        mesh = GetComponent<MeshFilter> ().mesh;
+    }
+
+    void Start()
+    {
+        MakeMeshData ();
+        CreateMesh ();
+    }
+
+    void MakeMeshData () {
+        vertices = new Vector3[]{ new Vector3(0,0,0), new Vector3(0,0,1), new Vector3(1,0,0) };
+        triangles = new int[] { 0, 1, 2 };
+    }
+
+    void CreateMesh () {
+        mesh.Clear ();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+}
