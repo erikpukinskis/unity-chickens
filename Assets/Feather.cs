8463bfb2ee49f1f6d6a1e43e107d4cd0d41f76dd
Erik Pukinskis <erik@snowedin.net>
2019-01-27 17:57:29 -0800

    Tried to render one vane of a feather and the geometry is pretty wonky

--- Assets/Feather.cs
+++ Assets/Feather.cs
@@ -9,6 +9,12 @@ public class Feather : MonoBehaviour
     Mesh mesh;
     Vector3[] vertices;
     int[] triangles;
+    int upWidth = 4;
+    int rachisBend = 2;
+    int downSweep = 2;
+    int upSweep = 6;
+    int tipWidth = 3;
+    int tipLength = 1;
 
     void Awake () {
         mesh = GetComponent<MeshFilter> ().mesh;
@@ -20,9 +26,35 @@ public class Feather : MonoBehaviour
         CreateMesh ();
     }
 
+
     void MakeMeshData () {
-        vertices = new Vector3[]{ new Vector3(0,0,0), new Vector3(0,0,1), new Vector3(1,0,0) };
-        triangles = new int[] { 0, 1, 2 };
+        vertices = new Vector3[] {
+            new Vector3(0,0,0),
+            new Vector3(upWidth, upSweep, 0),
+            new Vector3(0, rachisBend, 0),
+            new Vector3(tipWidth, downSweep, 0),
+            new Vector3(0, downSweep + tipLength)
+        };
+
+
+        int distalUmbilicus = 0;
+        int upEdge = 1;
+        int midRachis = 2; 
+        int downEdge = 3;
+        int tip = 4;
+
+        triangles = new int[] { distalUmbilicus,
+            upEdge,
+            midRachis,
+
+            midRachis,
+            upEdge,
+            downEdge,
+
+            midRachis,
+            downEdge,
+            tip,
+        };
     }
 
     void CreateMesh () {
