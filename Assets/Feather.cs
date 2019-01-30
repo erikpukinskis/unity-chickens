b86e310de83424de49bd6cd9325a41abd4809bd0
Erik Pukinskis <erik@snowedin.net>
2019-01-30 04:19:13 -0800

    Do things in a bigger integer-based world, and then scale down to fit in camera

--- Assets/Feather.cs
+++ Assets/Feather.cs
@@ -9,6 +9,8 @@ public class Feather : MonoBehaviour
     Mesh mesh;
     Vector3[] vertices;
     int[] triangles;
+    Vector3 scale;
+    Vector3 position;
 
     void Awake () {
         mesh = GetComponent<MeshFilter> ().mesh;
@@ -21,30 +23,46 @@ public class Feather : MonoBehaviour
     }
 
 
-    int upSweep = 2;
-    int upWidth = 4;
-    int rachisBend = 2;
-    int downSweep = 6;
-    int tipWidth = 3;
-    int length = 7;
+    int upSweep = 200;
+    int upWidth = 400;
+    int rachisBend = 200;
+    int downSweep = 600;
+    int tipWidth = 300;
+    int length = 700;
+    int curve = 100;
 
     void MakeMeshData () {
         vertices = new Vector3[] {
-            new Vector3(0,0,0),
-            new Vector3(0,0,rachisBend), // mid rachis
+            new Vector3(0,0,0), // distal umbilicus
+            new Vector3(0,curve,rachisBend), // mid rachis
             new Vector3(upWidth,0,upSweep), // up edge
             new Vector3(tipWidth,0,downSweep), // down edge
-            new Vector3(0,0,length)
+            new Vector3(0,0,length), // tip
+            new Vector3(-upWidth,0,upSweep), // up edge
+            new Vector3(-tipWidth,0,downSweep), // down edge
         };
 
+        scale = new Vector3(0.12f, 0.12f, 0.12f);
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].Scale(scale);
+        }
+
+        // scale vertices by 0.12745
+        // position -8, -70, 255
         int distalUmbilicus = 0;
         int midRachis = 1; 
         int upEdge = 2;
         int downEdge = 3;
         int tip = 4;
-        int leftUpEdge = 5;
+        int innerUpEdge = 5;
+        int innerDownEdge = 6;
 
         triangles = new int[] {
+
+            // outer vane
+
             distalUmbilicus,
             midRachis,
             upEdge,
@@ -56,36 +74,23 @@ public class Feather : MonoBehaviour
             tip,
             downEdge,
             midRachis,
-        };
-    }
 
-/*
-    void MakeMeshData () {
+            // inner vane
 
-vertices = new Vector3[] {
-    new Vector3(0,0,0),
-    new Vector3(upWidth, upSweep, 0),
-    new Vector3(0, rachisBend, 0),
-    new Vector3(tipWidth, downSweep, 0),
-    new Vector3(0, downSweep + tipLength)
-};
-
-        triangles = new int[] {
             distalUmbilicus,
-            upEdge,
+            innerUpEdge,
             midRachis,
 
             midRachis,
-            upEdge,
-            downEdge,
+            innerUpEdge,
+            innerDownEdge,
 
-            midRachis,
-            downEdge,
             tip,
+            midRachis,
+            innerDownEdge,
         };
     }
 
-*/
     void CreateMesh () {
         mesh.Clear ();
         mesh.vertices = vertices;
